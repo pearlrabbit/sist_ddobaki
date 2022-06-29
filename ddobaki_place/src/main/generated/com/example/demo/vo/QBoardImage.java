603b1d74c3013a562597a429fe97fcfbe8f039fd
Soyun Kim <89250252+iiolo@users.noreply.github.com>
2022-06-29 19:41:57 +0900

    Add files via upload

---
+++ ddobaki_place/src/main/generated/com/example/demo/vo/QBoardImage.java
@@ -0,0 +1,53 @@
+package com.example.demo.vo;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QBoardImage is a Querydsl query type for BoardImage
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QBoardImage extends EntityPathBase<BoardImage> {
+
+    private static final long serialVersionUID = 1652605776L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QBoardImage boardImage = new QBoardImage("boardImage");
+
+    public final QBoard board;
+
+    public final StringPath board_image_file = createString("board_image_file");
+
+    public final NumberPath<Integer> board_image_num = createNumber("board_image_num", Integer.class);
+
+    public QBoardImage(String variable) {
+        this(BoardImage.class, forVariable(variable), INITS);
+    }
+
+    public QBoardImage(Path<? extends BoardImage> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QBoardImage(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QBoardImage(PathMetadata metadata, PathInits inits) {
+        this(BoardImage.class, metadata, inits);
+    }
+
+    public QBoardImage(Class<? extends BoardImage> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.board = inits.isInitialized("board") ? new QBoard(forProperty("board"), inits.get("board")) : null;
+    }
+
+}
+
---
+++ ddobaki_place/src/main/java/com/example/demo/vo/PlaceSearchCondition.java
@@ -0,0 +1,26 @@
+package com.example.demo.vo;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PlaceSearchCondition {
+	private int	place_region_num;
+	
+	private int	place_type_num;	
+	
+	private String place_name;
+	
+	private int	place_park;	
+	
+	private int	place_spa;
+	
+	private int	place_meal;	
+	
+	private int	place_weight;
+	
+	private String place_array;
+}
+
